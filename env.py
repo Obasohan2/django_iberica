04851a5ae0e17bdfd5f45bfdfb4ae94617e06054
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-06-12 16:48:14 +0100

    Modified sttings.py and added database

---
+++ env.py
@@ -0,0 +1,3 @@
+import os
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_4kvWB6lKufGY@ep-round-dawn-a29xp2ab.eu-central-1.aws.neon.tech/grill_try_rush_867260"
+os.environ["SECRET_KEY"] = "t(x8+7v)_3vh9x0z05os17h6qqih*e@jp^e&b$%@_)!03qn(85"
