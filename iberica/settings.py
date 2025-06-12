8c232ee6f79ecacac8b45211698fd3ec34454425
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-06-12 17:22:25 +0100

    Add the heroku to allowed host and the recommented python-version

--- iberica/settings.py
+++ iberica/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'iberica.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'ibericablog-f1b21cd96563.herokuapp.com']
 
 
 # Application definition
