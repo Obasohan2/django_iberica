5786eeba7cde0fb644331d177c811047a3cdac21
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2025-06-14 16:05:31 +0100

    Added Category subpages, 404.html and wired views

--- blogApp/views.py
+++ blogApp/views.py
@@ -1,5 +1,39 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import get_object_or_404, redirect, render
+from .models import Post, Category  # adjust based on your app structure
 
 # Create your views here.
+
+
 def home(request):
-    return render(request, 'index.html')
+    categories = Category.objects.all()   # looping through the categories and passing them to templates
+    featured_posts = Post.objects.filter(is_featured=True, status='Published').order_by('updated_on')
+    posts = Post.objects.filter(is_featured=False, status='Published')
+    return render(request, 'index.html', {
+        'categories': categories,
+        'featured_posts': featured_posts,
+        'posts': posts
+    })
+
+
+def category_posts(request, category_id):
+    # fetch the posts that belongs to the category with the id category_id
+    posts = Post.objects.filter(status='Published', category=category_id)
+
+# Use try/except when we want to do some custom action if the category does not exists
+
+    # try:
+    # category = Category.objects.get(pk=category_id)
+
+    # except:
+    #     # redirect user to homepage
+    #     return redirect('home')
+
+# use get_object_or_404 when you want to show 404 error page if the category does not exist
+    category = get_object_or_404(Category, pk=category_id)
+
+    context = {
+      'posts':posts,
+      'category': category, 
+      }
+    return render(request, 'category_posts.html', context)
