97a77ebfb98dc5589ac51fbb7804135a2b9a20ea
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2026-02-02 18:26:35 +0000

    updated the post detail and button link functionality

--- blogApp/forms.py
+++ blogApp/forms.py
@@ -1,31 +1,65 @@
 from django import forms
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from .models import Post, Comment
 
 
-# class UserRegisterForm(UserCreationForm):
-#     email = forms.EmailField(required=True)
-
-#     class Meta:
-#         model = User
-#         fields = ['username', 'email', 'password1', 'password2']
-        
-        
-# class PostForm(forms.ModelForm):
-#     class Meta:
-#         model = Post
-#         fields = ('title', 'category', 'featured_image', 'excerpt', 'content', 'status', 'is_featured')
-        
-
 class CommentForm(forms.ModelForm):
+    body = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'rows': 4,
+                'placeholder': 'Write your comment here...'
+            }
+        ),
+        label=''
+    )
+
     class Meta:
         model = Comment
         fields = ('body',)
 
 
 class PostForm(forms.ModelForm):
+    title = forms.CharField(
+        widget=forms.TextInput(
+            attrs={
+                'class': 'form-control',
+                'placeholder': 'Post title'
+            }
+        )
+    )
+
+    content = forms.CharField(
+        widget=forms.Textarea(
+            attrs={
+                'class': 'form-control',
+                'rows': 10,
+                'placeholder': 'Write your post content here...'
+            }
+        )
+    )
+
+    category = forms.Select(
+        attrs={'class': 'form-control'}
+    )
+
+    status = forms.Select(
+        attrs={'class': 'form-control'}
+    )
+
+    featured_image = forms.ImageField(
+        required=False,
+        widget=forms.ClearableFileInput(
+            attrs={'class': 'form-control-file'}
+        )
+    )
+
     class Meta:
         model = Post
-        fields = ('title', 'content', 'category', 'featured_image', 'status',)
-
+        fields = (
+            'title',
+            'content',
+            'category',
+            'featured_image',
+            'status',
+        )
