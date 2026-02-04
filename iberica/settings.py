6fcd5955971a029d52ba4a9244b80afac715964f
Obasohan2 <jtp7d03u@students.codeinstitute.net>
2026-02-04 14:01:05 +0000

    Fix static files on Django 5 with WhiteNoise and STORAGES

--- iberica/settings.py
+++ iberica/settings.py
@@ -50,7 +50,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
-    'cloudinary_storage',      # Added for Cloudinary storage
+    # 'cloudinary_storage',      # Added for Cloudinary storage
     'django.contrib.staticfiles', 
     'cloudinary',        # Added for Cloudinary
     'django_summernote',
@@ -170,12 +170,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-
 MEDIA_URL = '/media/'
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STORAGES = {
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+}
+
+
 
 
 # Default primary key field type
